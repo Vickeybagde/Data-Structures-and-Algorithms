edc9db437b609a284a8a4c4043e83cf1d87bc9b3
Vickeybagde <vickeybagde799@gmail.com>
2025-08-20 12:35:22 +0530

    0-1 Triangle

---
+++ PATTERN PROBLEMS/ZeroOnetriangle.java
@@ -0,0 +1,21 @@
+public class ZeroOnetriangle {
+    
+    public static void  zero_1_triangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2 == 0){
+                    System.out.print("1"+  " ");
+                }
+                else{
+                    System.out.print("0" + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        zero_1_triangle(5);
+        
+    }
+}
