2280cea254bc4e494c1cc267dfe2371cde7444ba
Vickeybagde <vickeybagde799@gmail.com>
2025-09-13 02:22:40 +0530

    DSA

---
+++ Strings/Shortest.java
@@ -0,0 +1,25 @@
+public class Shortest {
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if (dir == 'N') {
+                y++;
+            } else if (dir == 'S') {
+                y--;
+            } else if (dir == 'E') {
+                x++;
+            } else if (dir == 'W') {
+                x--;
+            }
+        }
+
+        return (float)Math.sqrt((x * x) + (y * y));
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println("Shortest path = " + getShortestPath(path));
+    }
+}
